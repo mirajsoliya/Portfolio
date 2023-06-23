049d799e8a81a82aa5cb5a492fe37c7790ca0047
mirajsoliya <87265918+mirajsoliya@users.noreply.github.com>
2023-06-23 16:36:17 +0530

    Add files via upload

--- portfolio.js
+++ portfolio.js
@@ -1,61 +1,28 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyBbcGn0nlVtVxD9jWNcISSKSZTg2CN2gC0",
-    authDomain: "porfolio-5a487.firebaseapp.com",
-    databaseURL: "https://porfolio-5a487-default-rtdb.firebaseio.com",
-    projectId: "porfolio-5a487",
-    storageBucket: "porfolio-5a487.appspot.com",
-    messagingSenderId: "875537593851",
-    appId: "1:875537593851:web:9c2759a209ce2781b3875e",
-    measurementId: "G-DM2E9EZDTK"
-  };
- //initialize firebased
-  firebase.initializeApp(firebaseConfig);
-
-  //reference your database
-  var potfolioform = firebase.database().ref('portfolio');
-
-  document.getElementById('contactme').addEventListener("submit" , submitForm);
-
-  function submitForm(e)
+  function sendMail()
   {
-    e.preventDefault();
-    var name= getElementval("yourname");
-    var email=getElementval("youremail");
-    var subject = getElementval("yoursubject")
-    var msgContent=getElementval("msg");
-    saveMessage(name,email,subject,msgContent);
+    var params = {
+      name:document.getElementById('yourname').value,
+      email:document.getElementById('youremail').value,
+      message:document.getElementById('msg').value,
 
-    //enable alert
-    // document.querySelector('.alert').style.display="block";
-    swal("Done", "Message sent successfully!!", "success");
-    
-    //remove alert
-    // setTimeout(()=>{
+    };
 
-    //   document.querySelector('.alert').style.display="none";
 
-    // },3000);
+    const serviceId = "service_4cr1wtq";
+  const templateId="template_zhn5nnr";
 
-    //reset form
-
-    document.getElementById('contactme').reset();
+  emailjs.send(serviceId,templateId,params)
+  .then(
+    res=>{
+      document.getElementById("yourname").value="";
+      document.getElementById("youremail").value="";
+      document.getElementById("msg").value="";
+      console.log(res);
+      swal("Done", "Message sent successfully!!", "success");
+    }).catch(
+    err=>console.log(err));
 
 
   }
-  const saveMessage =(name,email,subject,msgContent)=>{
 
-    var newSubmitform = potfolioform.push();
-    newSubmitform.set({
-      name:name,
-      emailID : email,
-      Subject : subject,
-      message : msgContent,
-
-    });
-
-
-  };
-
-  const getElementval = (id)=>{
-    return document.getElementById(id).value;
-  }
+  
